447862d8bc76f9a7b859b850789e7620101ba54e
Ryan Schmelter <rschmelter33@gmail.com>
2018-08-23 00:31:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,20 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  return document.querySelector('#nested .target')
+  return document.querySelector('#nested .target');
+  
+}
+
+function increaseRankBy(n) {
+  const ranked = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0, l = ranked.length; i < l; i++) {
+    let children = ranked[i].children
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
+
+    
+  }
 }
